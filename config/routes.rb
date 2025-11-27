ccc9ed8cec4c81b25b334c2ac899418c921b4e1d
snap112040385 <petrsergeev210608@gmail.com>
2025-11-27 17:15:01 +0400

    заменил gtm 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "landing/index"
   get "landing/sasha"
-  get "landing/LooserVibeCoder", to: "landing#looser_vibe_coder"
+  get "landing/fitbot", to: "landing#looser_vibe_coder"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
